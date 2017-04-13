8ef3f08e1beb89fe3d9bf45b4e6e228c8838703b
Robbie Lopez <rolopez.email@gmail.com>
2017-04-12 22:02:37 -0600

    basic layout

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+
+
+
+		<title>HTML SANDBOX</title>
+	</head>
+	<body>
+
+		<header>
+			hello world
+		</header>
+		<main>
+
+hello
+		</main>
+
+	</body>
+</html>
